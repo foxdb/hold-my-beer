63d669529f7f0278c0f9a1276a730a0bd929ccbc
foxdb <bens.schmitt@gmail.com>
2019-10-21 08:28:33 +1000

    moved ProjectSelector in its own component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
 import clsx from 'clsx'
-import { emphasize, makeStyles, useTheme, fade } from '@material-ui/core/styles'
+import { makeStyles, fade } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import * as PropTypes from 'prop-types'
-import TextField from '@material-ui/core/TextField'
 
 import Drawer from '@material-ui/core/Drawer'
-import NoSsr from '@material-ui/core/NoSsr'
 
 import AppBar from '@material-ui/core/AppBar'
-import Select from 'react-select'
-
-import MenuItem from '@material-ui/core/MenuItem'
 
 import Toolbar from '@material-ui/core/Toolbar'
 // import List from '@material-ui/core/List'
@@ -24,6 +18,8 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import MenuIcon from '@material-ui/icons/Menu'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+
+import ProjectSelector from '../components/ProjectSelector'
 // import InputBase from '@material-ui/core/InputBase'
 // import NotificationsIcon from '@material-ui/icons/Notifications'
 // import { mainListItems, secondaryListItems } from './listItems'
@@ -63,6 +59,9 @@ const useStyles = makeStyles(theme => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     })
+  },
+  fixedHeight: {
+    height: 240
   },
   appBarShift: {
     marginLeft: drawerWidth,
@@ -142,320 +141,16 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column'
-  },
-  // below: Select styles
-  menuPaper: {
-    position: 'absolute',
-    zIndex: 1,
-    marginTop: theme.spacing(1),
-    left: 0,
-    right: 0
-  },
-  selectRoot: {
-    // flexGrow: 1,
-    // height: 250,
-    minWidth: 290
-  },
-  selectInput: {
-    display: 'flex',
-    padding: 0,
-    height: 'auto'
-  },
-  valueContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flex: 1,
-    alignItems: 'center',
-    overflow: 'hidden'
-  },
-  chip: {
-    margin: theme.spacing(0.5, 0.25)
-  },
-  chipFocused: {
-    backgroundColor: emphasize(
-      theme.palette.type === 'light'
-        ? theme.palette.grey[300]
-        : theme.palette.grey[700],
-      0.08
-    )
-  },
-  noOptionsMessage: {
-    padding: theme.spacing(1, 2)
-  },
-  singleValue: {
-    fontSize: 16,
-    color: 'white'
-  },
-  fixedHeight: {
-    height: 240
-  },
-  placeholder: {
-    position: 'absolute',
-    left: 2,
-    bottom: 6,
-    fontSize: 16
-  },
-  divider: {
-    height: theme.spacing(2)
   }
 }))
 
-function NoOptionsMessage(props) {
-  return (
-    <Typography
-      color="textSecondary"
-      className={props.selectProps.classes.noOptionsMessage}
-      {...props.innerProps}
-    >
-      {props.children}
-    </Typography>
-  )
-}
-
-NoOptionsMessage.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.node,
-  /**
-   * Props to be passed on to the wrapper.
-   */
-  innerProps: PropTypes.object.isRequired,
-  selectProps: PropTypes.object.isRequired
-}
-
-function inputComponent({ inputRef, ...props }) {
-  return <div ref={inputRef} {...props} />
-}
-
-inputComponent.propTypes = {
-  inputRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({
-      current: PropTypes.any.isRequired
-    })
-  ])
-}
-
-function Control(props) {
-  const {
-    children,
-    innerProps,
-    innerRef,
-    selectProps: { classes, TextFieldProps }
-  } = props
-
-  return (
-    <TextField
-      fullWidth
-      InputProps={{
-        inputComponent,
-        inputProps: {
-          className: classes.selectInput,
-          ref: innerRef,
-          children,
-          ...innerProps
-        }
-      }}
-      {...TextFieldProps}
-    />
-  )
-}
-
-Control.propTypes = {
-  /**
-   * Children to render.
-   */
-  children: PropTypes.node,
-  /**
-   * The mouse down event and the innerRef to pass down to the controller element.
-   */
-  innerProps: PropTypes.shape({
-    onMouseDown: PropTypes.func.isRequired
-  }).isRequired,
-  innerRef: PropTypes.oneOfType([
-    PropTypes.oneOf([null]),
-    PropTypes.func,
-    PropTypes.shape({
-      current: PropTypes.any.isRequired
-    })
-  ]).isRequired,
-  selectProps: PropTypes.object.isRequired
-}
-
-function Option(props) {
-  return (
-    <MenuItem
-      ref={props.innerRef}
-      selected={props.isFocused}
-      component="div"
-      style={{
-        fontWeight: props.isSelected ? 500 : 400
-      }}
-      {...props.innerProps}
-    >
-      {props.children}
-    </MenuItem>
-  )
-}
-
-Option.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.node,
-  /**
-   * props passed to the wrapping element for the group.
-   */
-  innerProps: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    key: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-    onMouseMove: PropTypes.func.isRequired,
-    onMouseOver: PropTypes.func.isRequired,
-    tabIndex: PropTypes.number.isRequired
-  }).isRequired,
-  /**
-   * Inner ref to DOM Node
-   */
-  innerRef: PropTypes.oneOfType([
-    PropTypes.oneOf([null]),
-    PropTypes.func,
-    PropTypes.shape({
-      current: PropTypes.any.isRequired
-    })
-  ]).isRequired,
-  /**
-   * Whether the option is focused.
-   */
-  isFocused: PropTypes.bool.isRequired,
-  /**
-   * Whether the option is selected.
-   */
-  isSelected: PropTypes.bool.isRequired
-}
-
-function Placeholder(props) {
-  const { selectProps, innerProps = {}, children } = props
-  return (
-    <Typography
-      color="textSecondary"
-      className={selectProps.classes.placeholder}
-      {...innerProps}
-    >
-      {children}
-    </Typography>
-  )
-}
-
-Placeholder.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.node,
-  /**
-   * props passed to the wrapping element for the group.
-   */
-  innerProps: PropTypes.object,
-  selectProps: PropTypes.object.isRequired
-}
-
-function SingleValue(props) {
-  return (
-    <Typography
-      className={props.selectProps.classes.singleValue}
-      {...props.innerProps}
-    >
-      {props.children}
-    </Typography>
-  )
-}
-
-SingleValue.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.node,
-  /**
-   * Props passed to the wrapping element for the group.
-   */
-  innerProps: PropTypes.any.isRequired,
-  selectProps: PropTypes.object.isRequired
-}
-
-function ValueContainer(props) {
-  return (
-    <div className={props.selectProps.classes.valueContainer}>
-      {props.children}
-    </div>
-  )
-}
-
-ValueContainer.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.node,
-  selectProps: PropTypes.object.isRequired
-}
-
-function Menu(props) {
-  return (
-    <Paper
-      square
-      className={props.selectProps.classes.menuPaper}
-      {...props.innerProps}
-    >
-      {props.children}
-    </Paper>
-  )
-}
-
-Menu.propTypes = {
-  /**
-   * The children to be rendered.
-   */
-  children: PropTypes.element.isRequired,
-  /**
-   * Props to be passed to the menu wrapper.
-   */
-  innerProps: PropTypes.object.isRequired,
-  selectProps: PropTypes.object.isRequired
-}
-
-const components = {
-  Control,
-  Menu,
-  NoOptionsMessage,
-  Option,
-  Placeholder,
-  SingleValue,
-  ValueContainer
-}
-
 export default function Dashboard() {
   const classes = useStyles()
-  const theme = useTheme()
 
-  const selectStyles = {
-    input: base => ({
-      ...base,
-      color: theme.palette.text.primary,
-      '& input': {
-        font: 'inherit'
-      }
-    })
-  }
-
-  const logFileOptions = ['test 1 ben', 'manon', 'another test'].map(file => ({
-    value: file,
-    label: file
-  }))
+  const logFiles = ['test 1 ben', 'manon', 'another test']
 
   const [open, setOpen] = React.useState(false)
   const [selectedLogFile, setSelectedLogfile] = React.useState(null)
-
-  // const [logFiles, setLogFiles] = React.useState([])
 
   const handleDrawerOpen = () => {
     setOpen(true)
@@ -488,10 +183,6 @@ export default function Dashboard() {
 
   // }
 
-  const handleLogFileSelect = value => {
-    setSelectedLogfile(value)
-  }
-
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
 
   return (
@@ -523,26 +214,11 @@ export default function Dashboard() {
           >
             Hold my Beer
           </Typography>
-          <div className={classes.selectRoot}>
-            <NoSsr>
-              <Select
-                placeholder="Select a project"
-                inputId="select-project"
-                TextFieldProps={{
-                  InputLabelProps: {
-                    htmlFor: 'react-select-single',
-                    shrink: true
-                  }
-                }}
-                classes={classes}
-                styles={selectStyles}
-                value={selectedLogFile}
-                onChange={handleLogFileSelect}
-                options={logFileOptions}
-                components={components}
-              />
-            </NoSsr>
-          </div>
+          <ProjectSelector
+            logFiles={logFiles}
+            selectedLogFile={selectedLogFile}
+            setSelectedLogFile={setSelectedLogfile}
+          />
         </Toolbar>
       </AppBar>
       <Drawer
