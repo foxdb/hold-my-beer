f9b8f987f37070a5f9088b75327c288f747e530d
Benjamin <bens.schmitt@gmail.com>
2020-06-24 11:52:14 +1000

    add some logging to syncSchemas

--- api/src/endpoints/syncSchemas.ts
+++ api/src/endpoints/syncSchemas.ts
@@ -2,7 +2,9 @@ import * as db from '../lib/db'
 
 export default async (event, context) => {
   try {
+    console.log('attempting to sync DB schemas...')
     await db.syncSchemas()
+    console.log('schemas are now in sync...')
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*' },
