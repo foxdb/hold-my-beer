b518e2d76e840b832d34702fefa09a16461a0b8c
Benjamin <bens.schmitt@gmail.com>
2021-04-23 09:36:24 +1000

    fix: trigger build

--- api/src/endpoints/hello.ts
+++ api/src/endpoints/hello.ts
@@ -2,7 +2,7 @@ export default async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from HMB API',
+      message: 'Hello from HMB API!',
     }),
     headers: {
       'Access-Control-Allow-Origin': '*',
