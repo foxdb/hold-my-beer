b2dd58d3582be0308132d605fa26183f32862562
Benjamin <bens.schmitt@gmail.com>
2021-04-23 10:32:10 +1000

    fix tests

--- api/src/endpoints/hello.spec.ts
+++ api/src/endpoints/hello.spec.ts
@@ -3,5 +3,5 @@ import hello from './hello'
 test('hello handler returns 200', async () => {
   const response = await hello({}, {})
   expect(response.statusCode).toBe(200)
-  expect(JSON.parse(response.body).message).toBe('Hello from HMB API')
+  expect(JSON.parse(response.body).message).toBe('Hello from HMB API!')
 })
